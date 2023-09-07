601560da4ea3099523684dfc6e737310569cc831
Gustavo Andres Romero Ordoñez <gus.romero.ordonez@outlook.com>
2023-09-07 03:13:28 -0500

    The scraping was developed

--- Controllers/news_controller.py
+++ Controllers/news_controller.py
@@ -1,7 +1,13 @@
 from app import app
-from flask import request
+from flask import jsonify, request
+from Domain.Services.news_service import News_service
+
+service = News_service() 
 
 @app.route("/news", methods=["GET"])
 def get_news_by_topic():
-  topic = request.args['topic']
-  return topic
+  return service.scrape_news_by_topic(request.args['topic'])
+
+@app.route("/news/available", methods=["GET"])
+def get_available_news_papers(): 
+  return jsonify(service.get_available_news_papers())
